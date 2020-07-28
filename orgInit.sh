d7c0947d0bfd762ac0e3288b3d64296115011a45
mshanemc <shane.mclaughlin@salesforce.com>
2020-07-28 09:55:57 -0500

    assign to user properly

--- orgInit.sh
+++ orgInit.sh
@@ -1,5 +1,5 @@
 sfdx shane:org:create -f config/project-scratch-def.json -d 1 -s --userprefix notify -o work.shop
 sfdx force:source:push
 sfdx force:user:permset:assign -n Notifications
-sfdx shane:user:password:set -p workshop
+sfdx shane:user:password:set -p workshop -g User -l User
 sfdx force:org:open
