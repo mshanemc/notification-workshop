c03b94c395553bf0d372564f1a24127d22a2cff2
mshanemc <shane.mclaughlin@salesforce.com>
2020-07-28 09:33:21 -0500

    plus friendly username

--- orgInit.sh
+++ orgInit.sh
@@ -1,4 +1,4 @@
-sfdx force:org:create -f config/project-scratch-def.json -d 1 -s
+sfdx shane:org:create -f config/project-scratch-def.json -d 1 -s --userprefix notify -o work.shop
 sfdx force:source:push
 sfdx force:user:permset:assign -n Notifications
 sfdx shane:user:password:set -p workshop
