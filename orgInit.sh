89fbd6c50cddc740d7a14e5a7f60e7bd22b5588b
mshanemc <shane.mclaughlin@salesforce.com>
2020-07-28 09:32:18 -0500

    we'll also need a password

--- orgInit.sh
+++ orgInit.sh
@@ -1,4 +1,5 @@
 sfdx force:org:create -f config/project-scratch-def.json -d 1 -s
 sfdx force:source:push
 sfdx force:user:permset:assign -n Notifications
+sfdx shane:user:password:set -p workshop
 sfdx force:org:open
